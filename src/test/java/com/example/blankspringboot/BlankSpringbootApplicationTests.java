253baa27c6b3c4c01027a89bdbf3c7c78a2d3157
anugrahzeputra <anugrahzeputra@gmail.com>
2026-01-19 11:52:19 +0700

    initial init

---
+++ src/test/java/com/example/blankspringboot/BlankSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blankspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
